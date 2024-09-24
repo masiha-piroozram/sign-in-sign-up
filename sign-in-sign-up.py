b0154c81e5f6021950efd4c081c1d4ef3374b27e
Masiha Piroozram <masiihap@gmail.com>
2024-09-24 19:47:46 +0330

    Update sign-in-sign-up.py

--- sign-in-sign-up.py
+++ sign-in-sign-up.py
@@ -1,284 +1,194 @@
-#___________________________________________________________________________________________________________________________________________________
-def attacker():
-    import cv2
-    from ListOfUsers import user
+import hashlib
+import os
+import tkinter as tk
+from tkinter import messagebox, simpledialog
 
-    pic = cv2.imread('%s%d.jpg' % (user[i] , i))
-    cv2.imshow('Picture of attacker' , pic)
-    k = cv2.waitKey(300)
-    cv2.destroyWindow('Picture of attacker')
-    
-#___________________________________________________________________________________________________________________________________________________
-def SendPic():
-    import os
-    import smtplib
-    from email.mime.text import MIMEText
-    from email.mime.image import MIMEImage
-    from email.mime.multipart import MIMEMultipart
-    img_data = open('%s%d.jpg' %(user[i] , i) , 'rb').read()
-    msg = MIMEMultipart()
-    msg['Subject'] = 'subject'
-    msg['From'] = 'auto.email.python1@gmail.com'
-    msg['To'] = ('%s' % email[i])
+# Utility functions
+def hash_password(password):
+    """Hashes a password using SHA-256."""
+    return hashlib.sha256(password.encode()).hexdigest()
 
-    text = MIMEText("Login attemp!!!")
-    msg.attach(text)
-    image = MIMEImage(img_data, name=os.path.basename('masiha0.jpg'))
-    msg.attach(image)
+def save_user(username, password_hash, phone_number):
+    """Saves the username, hashed password, and phone number to a file."""
+    with open('users.txt', 'a') as file:
+        file.write(f"{username},{password_hash},{phone_number}\n")
 
-    s = smtplib.SMTP('smtp.gmail.com:587')
-    s.ehlo()
-    s.starttls()
-    s.ehlo()
-    s.login('auto.email.python1@gmail.com', 'prnxkayqigoznywl')
-    s.sendmail('auto.email.python1@gmail.com', '%s' %email[i], msg.as_string())
-    s.quit()
+def user_exists(username):
+    """Checks if a username already exists in the file."""
+    if not os.path.exists('users.txt'):
+        return False
+    with open('users.txt', 'r') as file:
+        for line in file:
+            stored_username, _, _ = line.strip().split(',')
+            if stored_username == username:
+                return True
+    return False
 
+def validate_user(username, password):
+    """Validates a user's credentials."""
+    if not os.path.exists('users.txt'):
+        return False
+    password_hash = hash_password(password)
+    with open('users.txt', 'r') as file:
+        for line in file:
+            stored_username, stored_password_hash, _ = line.strip().split(',')
+            if stored_username == username and stored_password_hash == password_hash:
+                return True
+    return False
 
-#___________________________________________________________________________________________________________________________________________________
-def takePic():
-    import numpy as np
-    import time 
-    import cv2
-    import datetime
-    camera_port = 2
-    camera = cv2.VideoCapture(camera_port)
-    time.sleep(0.1)
-    return_value, image = camera.read()
-    #Now lets write a text on the pic that tells us what have happened
-    font                   = cv2.FONT_HERSHEY_SIMPLEX
-    bottomLeftCornerOfText = (50,300)
-    fontScale              = 1
-    fontColor              = (255,255,255)
-    lineType               = 2
+def get_phone_number(username):
+    """Retrieves the phone number for a given username."""
+    if not os.path.exists('users.txt'):
+        return None
+    with open('users.txt', 'r') as file:
+        for line in file:
+            stored_username, _, phone_number = line.strip().split(',')
+            if stored_username == username:
+                return phone_number
+    return None
 
-    
- 
-    dt= datetime.datetime.now()
-    dateANDtime = str('%d-%d-%d   %d:%d:%d' %(dt.year , dt.month , dt.day , dt.hour , dt.minute , dt.second))
-
-    cv2.putText(image,'Login attemp from this person at   %s' % (dateANDtime) , 
-    bottomLeftCornerOfText, 
-    font, 
-    fontScale,
-    fontColor,
-    lineType)
-    
-    cv2.imwrite("%s%d.jpg" %(user[i] , i) , image)
-    del(camera)
-#__________________________________________________________________________________________________________________________________________________
-
-def RecoveryPass():
-    recovery = int(input('You enter wrong password three times.\nFor getting a password recovery enter "1"\nFor answering security questions enter "2"\n'))
-    if recovery == 1:
-    #The code from this line to the "End tag" ,is the code fore sending email 
-
-
-        RecoveryEmail()
-
-
-    #End tag
-        print("we've send a recovery link to",email[i])
-        exit()
-    if recovery == 2:
-
-
-        SecurityQuestion()
-
-                        
-        exit()
-
-
-#________________________________________________________________________________________________________________________________________________________
-#CALCULATOR
-
-def summ():
-    x = int(input('\nEnter two numbers : '))
-    y = int(input())
-    print('\n%s + %s = %s' %(x,y,x+y))
-    sumlog = open('ListOfUsers.py','a')
-    sumlog.write("\nLogMath[%s].append('%s + %s = %s')"  % (i,x,y,x+y))
-    sumlog.close()
-
-def mult():
-    x = int(input('\nEnter two numbers : '))
-    y = int(input())
-    print('\n%s X %s = %s' %(x,y,x*y))
-    multlog = open('ListOfUsers.py','a')
-    multlog.write("\nLogMath[%s].append('%s X %s = %s')" % (i,x,y,x*y))
-    multlog.close()
-
-def div():
-    x = int(input('\nEnter two numbers : '))
-    y = int(input())
-    print('\n%s / %s = %s' %(x,y,float(x/y)))
-    divlog = open('ListOfUsers.py','a')
-    divlog.write("\nLogMath[%s].append('%s / %s = %s')" % (i,x,y,float(x/y)))
-    divlog.close()
-
-def integral():
-    import webbrowser
-    webbrowser.open('https://www.integral-calculator.com')
-    
-def deriative():
-    import webbrowser
-    webbrowser.open('https://www.derivative-calculator.net')
-
-def matrix():
-    import webbrowser
-    webbrowser.open('https://matrixcalc.org/en/')
-
-#___________________________________________________________________________________________________________________________________________________________
-
-def AfterLogin():
-    print('What do you want to do?\n\n1.Calculate the summation of two numbers\n\n2.Calculate the multiple of two numbers\n\n3.Calculate the division of\
-two number\n\n4.Integral CalCulator\n\n5.Deriative Calculator\n\n6.Matrix Calculator\n\n7.See the log of wrong passwords\n\n\
-8.See the picture of the person who attempted to login\n\n9.See the log of previous calculations\n\n10.EXIT \n')
-    choice = input('Enter the number of your choice : ')
-    if choice == '1':
-        summ()
-    elif choice == '2':
-        mult()
-    elif choice =='3':
-        div()
-    elif choice =='4':
-        integral()
-    elif choice =='5':
-        deriative()
-    elif choice == '6':
-        matrix()
-    elif choice == '7':
-        from ListOfUsers import Log
-        print(Log[i])
-    elif choice == '8':
-        attacker()
-    elif choice == '9':
-        from ListOfUsers import LogMath
-        print(LogMath[i])
-    elif choice == '10':
-        exit()
-    else:
-        print("Please enter a valid number!")
-
-    nextStep = input('\nDo you want to continue?(y/n)\n')
-    if nextStep == 'y':
-        AfterLogin()
-    elif nextStep == 'n':
-        print('Good Bye')
-        exit()
-
-
-#________________________________________________________________________________________________________________________________
-def SecurityQuestion():
-    answer1 = input('Where did your parents meet?')
-    answer2 = input('What is your favorite color?')
-    if answer1 == secques1[i] and answer2 == secques2[i]:
-        print('The password that you forgot is :' , Pass[i],'\n','Welcome ',user[i])
-        exit()
-    else:
-        print('Due to security terms , unfortunately this account is blocked.')
-#_________________________________________________________________________________________________________________________________
-def SignUp():
-    emptyTest = 0
-        #emptyTest is an identifire to know from which argument we don't have user and we should add our new user to it
-        #The code lines from the 'while' below to the next tag are code lines for adding new user to our ListOfusers
-    while user[emptyTest] != 'empty':
-        emptyTest = emptyTest + 1
-
-    newuser = open('ListOfUsers.py','a')
-        
-    nUser = input('\nEnter your Username :')
-    newuser.write("\nuser[%s] = '%s';" % (emptyTest , nUser))
-        
-    nPass = input('\nEnter your Password :')
-    newuser.write("Pass[%s] = '%s';" % (emptyTest , nPass))
-        
-    nEmail = input('\nEnter your email address :')
-    newuser.write("email[%s] = '%s';" % (emptyTest , nEmail))
-        
-    print('\nFor more security ,please answer these 2 security questions.')
-
-    nsecques1 = input('\nWhere did your parents meet ?')
-    newuser.write("secques1[%s] = '%s';" % (emptyTest , nsecques1))
-        
-    nsecques2 = input('\nWhat is your favorite color ?')
-    newuser.write("secques2[%s] = '%s';" % (emptyTest , nsecques2))
-    #The end of code lines of adding new user 
-    print('\nThanks for signing up :)')
-    newuser.close()
-#_________________________________________________________________________________________________________________________________
-def RecoveryEmail():
-    import smtplib
-    from ListOfUsers import email
-    from ListOfUsers import Pass
-    content = ('Hi\nThis is a password recovery email\nYour password is : %s' % (Pass[i]))
-    mail = smtplib.SMTP('smtp.gmail.com:587')
-    mail.ehlo()
-    mail.starttls()
-    mail.login('auto.email.python1@gmail.com','statdesigner')
-    mail.sendmail('auto.email.python1@gmail.com',email[i],content)
-    mail.close()
-#_________________________________________________________________________________________________________________________________   
-###########
-###########
-###########
-
-from ListOfUsers import user
-from ListOfUsers import Pass
-from ListOfUsers import email
-from ListOfUsers import secques1
-from ListOfUsers import secques2
-
-
-name = input('Enter your username : ')
-fchecker = 1
-emptyval_checker = 0
-for i in range(len(user)):
-    if name == user[i]:
-        for password in range(3):
-            
-            password = input('Enter your password :')
-            
-            if password == Pass[i]:
-                print('Welcome',user[i],'\n')
-                AfterLogin()
-                exit()
-            elif password == '':
-
-                if emptyval_checker < 3:
-                    print("\nYou can't enter empty value , try again.")
-                else:
-
-                    takePic()
-
-                    SendPic()
-                    
-                    RecoveryPass()
-
+def reset_password(username, phone_number, new_password):
+    """Resets the user's password if the phone number matches."""
+    if not os.path.exists('users.txt'):
+        return "User not found."
+    new_password_hash = hash_password(new_password)
+    lines = []
+    with open('users.txt', 'r') as file:
+        for line in file:
+            stored_username, stored_password_hash, stored_phone_number = line.strip().split(',')
+            if stored_username == username and stored_phone_number == phone_number:
+                lines.append(f"{username},{new_password_hash},{phone_number}\n")
             else:
-                log = open('ListOfUsers.py','a')
-                log.write("\nLog[%s].append('%s')" %(i,password))
-                log.close()
-                if fchecker < 3:
-                    print('Wrong password , try agin.')
-                    fchecker = fchecker + 1
-                else:
+                lines.append(line)
+    with open('users.txt', 'w') as file:
+        file.writelines(lines)
+    return "Password reset successfully."
 
-                    takePic()
+def sign_up(username, password, phone_number):
+    """Handles user sign-up."""
+    if user_exists(username):
+        return "Username already exists. Please choose a different username."
+    if len(password) < 6:
+        return "Password must be at least 6 characters long."
+    if not phone_number.startswith("09") or len(phone_number) != 11:
+        return "Phone number must start with '09' and be 11 characters long."
+    password_hash = hash_password(password)
+    save_user(username, password_hash, phone_number)
+    return "User signed up successfully."
 
-                    SendPic()
-                    
-                    RecoveryPass()
-                        
-else:
-    SignUpQ = input("\nYou don't have an account yet.\nDo you want to sign up?\n\nYes?(Enter 'y')\nNo?(Enter anything but 'y')\n")
-    if SignUpQ == 'y':
+def sign_in(username, password):
+    """Handles user sign-in."""
+    if not user_exists(username):
+        return "You don't have an account. Please sign up first."
+    if validate_user(username, password):
+        return "Sign-in successful."
+    return "Invalid username or password."
 
+# GUI functions
+def sign_up_gui():
+    def validate_sign_up():
+        username = username_entry.get()
+        password = password_entry.get()
+        phone_number = phone_number_entry.get()
+        valid = True
 
-        SignUp()
+        if user_exists(username):
+            username_error.config(text="Username already exists.")
+            valid = False
+        else:
+            username_error.config(text="")
 
+        if len(password) < 6:
+            password_error.config(text="Password must be at least 6 characters long.")
+            valid = False
+        else:
+            password_error.config(text="")
 
-        exit()
-    else:
-        print('\nGood Bye')
-        exit()
+        if not phone_number.startswith("09") or len(phone_number) != 11:
+            phone_number_error.config(text="Phone number must start with '09' and be 11 characters long.")
+            valid = False
+        else:
+            phone_number_error.config(text="")
+
+        if valid:
+            message = sign_up(username, password, phone_number)
+            messagebox.showinfo("Sign Up", message)
+            sign_up_window.destroy()
+
+    sign_up_window = tk.Toplevel(root)
+    sign_up_window.title("Sign Up")
+
+    tk.Label(sign_up_window, text="Username:").grid(row=0, column=0, padx=10, pady=5)
+    username_entry = tk.Entry(sign_up_window)
+    username_entry.grid(row=0, column=1, padx=10, pady=5)
+    username_error = tk.Label(sign_up_window, text="", fg="red")
+    username_error.grid(row=0, column=2, padx=10, pady=5)
+
+    tk.Label(sign_up_window, text="Password:").grid(row=1, column=0, padx=10, pady=5)
+    password_entry = tk.Entry(sign_up_window, show='*')
+    password_entry.grid(row=1, column=1, padx=10, pady=5)
+    password_error = tk.Label(sign_up_window, text="", fg="red")
+    password_error.grid(row=1, column=2, padx=10, pady=5)
+
+    tk.Label(sign_up_window, text="Phone Number:").grid(row=2, column=0, padx=10, pady=5)
+    phone_number_entry = tk.Entry(sign_up_window)
+    phone_number_entry.grid(row=2, column=1, padx=10, pady=5)
+    phone_number_error = tk.Label(sign_up_window, text="", fg="red")
+    phone_number_error.grid(row=2, column=2, padx=10, pady=5)
+
+    tk.Button(sign_up_window, text="Sign Up", command=validate_sign_up).grid(row=3, column=1, pady=10)
+
+def sign_in_gui():
+    def validate_sign_in():
+        username = username_entry.get()
+        password = password_entry.get()
+        attempts = 0
+
+        if not user_exists(username):
+            messagebox.showerror("Sign In", "You don't have an account. Please sign up first.")
+            sign_in_window.destroy()
+            return
+
+        while attempts < 3:
+            message = sign_in(username, password)
+            if message == "Sign-in successful.":
+                messagebox.showinfo("Sign In", message)
+                sign_in_window.destroy()
+                return
+            else:
+                attempts += 1
+                password = simpledialog.askstring("Sign In", f"Invalid password. Attempt {attempts}/3. Enter password again:", show='*')
+
+        phone_number = simpledialog.askstring("Reset Password", "Enter phone number to reset password:")
+        stored_phone_number = get_phone_number(username)
+        if phone_number == stored_phone_number:
+            new_password = simpledialog.askstring("Reset Password", "Enter new password:", show='*')
+            message = reset_password(username, phone_number, new_password)
+            messagebox.showinfo("Reset Password", message)
+        else:
+            messagebox.showerror("Reset Password", "Phone number does not match.")
+
+    sign_in_window = tk.Toplevel(root)
+    sign_in_window.title("Sign In")
+
+    tk.Label(sign_in_window, text="Username:").grid(row=0, column=0, padx=10, pady=5)
+    username_entry = tk.Entry(sign_in_window)
+    username_entry.grid(row=0, column=1, padx=10, pady=5)
+
+    tk.Label(sign_in_window, text="Password:").grid(row=1, column=0, padx=10, pady=5)
+    password_entry = tk.Entry(sign_in_window, show='*')
+    password_entry.grid(row=1, column=1, padx=10, pady=5)
+
+    tk.Button(sign_in_window, text="Sign In", command=validate_sign_in).grid(row=2, column=1, pady=10)
+
+# Main GUI
+root = tk.Tk()
+root.title("Sign In/Sign Up System")
+
+sign_up_button = tk.Button(root, text="Sign Up", command=sign_up_gui)
+sign_up_button.pack(pady=10)
+
+sign_in_button = tk.Button(root, text="Sign In", command=sign_in_gui)
+sign_in_button.pack(pady=10)
+
+root.mainloop()
